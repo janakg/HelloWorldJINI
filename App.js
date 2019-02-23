9f22603ee6905dbc4261ef5e237c036dc5d6238d
Ram Ganesan <ramg8x@gmail.com>
2019-02-23 12:59:08 +0530

    base code + Android working

---
+++ App.js
@@ -0,0 +1,89 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NativeModules,
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value1: 10,
+      value2: 5,
+      sum: 'Loading...',
+    };
+    this.calculateSum();
+  }
+
+  calculateSum = () => {
+    const { HelloWorld } = NativeModules;
+
+    // Function "HelloWorld.add(...)" is implemented in c++
+    HelloWorld
+      .add(this.state.value1, this.state.value2)
+      .then(sum => {
+        const newState = Object.assign({}, this.state, { sum });
+        this.setState(newState);
+      });
+  };
+
+
+  render() {
+    return (
+       <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+        <Text style={styles.instructions}>
+          {this.state.value1} + {this.state.value2} = {this.state.sum}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ src/cpp/hello_world_impl.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "hello_world.hpp"
+
+namespace helloworld {
+
+    class HelloWorldImpl : public helloworld::HelloWorld {
+
+    public:
+
+        // Constructor
+        HelloWorldImpl();
+
+        // Our method that returns a string
+        std::string get_hello_world();
+
+        int add(int a, int b);
+
+    };
+
+}
+
